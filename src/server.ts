da4cf62ef2c1340dac8f8888f602543cc953b8e0
Aliyu <sadiqful@gmail.com>
2023-06-16 20:52:58 +0100

    changes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express, { response } from "express";
 import connectToDatabase from './db'
 import dotenv from "dotenv"
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express();
 
 const PORT = process.env.PORT || 3000
+
+app.use(cors());
+
+connectToDatabase()
 
 app.get("/ping", (request, response) => {
     response.send("Server works fine")
